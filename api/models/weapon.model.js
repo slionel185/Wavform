4996f7b544061b35a6e31b0313ebdc015ae5cc18
slionel185 <slionel1850@gmail.com>
2020-04-29 09:26:58 -0700

    Add files via upload

--- api/models/weapon.model.js
+++ api/models/weapon.model.js
@@ -1,1 +1,125 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
+const weaponSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    weaponType: {
+        type: String,
+        required: true
+    },
+    frame: {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        }
+    },
+    shownStats: {
+        impact: {
+            type: Number,
+            required: true
+        },
+        range: {
+            type: Number,
+            required: true
+        },
+        stability: {
+            type: Number,
+            required: true
+        },
+        handling: {
+            type: Number,
+            required: true
+        },
+        reloadSpeed: {
+            type: Number,
+            required: true
+        }
+    },
+    hiddenStats: {
+        aimAssistance: {
+            type: Number,
+            required: true
+        },
+        inventorySize: {
+            type: Number,
+            required: true
+        },
+        zoom: {
+            type: Number,
+            required: true
+        },
+        recoil: {
+            type: Number,
+            required: true
+        },
+        bounceIntensity: {
+            type: Number,
+            required: true
+        },
+        bounceDirection: {
+            type: String,
+            required: true
+        }
+    },
+    curatedRoll: {
+        barrelPerk: {
+            type: String,
+            required: false
+        },
+        magPerk: {
+            type: String,
+            required: false
+        },
+        perk1: {
+            type: String,
+            required: false
+        },
+        perk2: {
+            type: String,
+            required: false
+        },
+        masterwork: {
+            type: String,
+            required: false
+        }
+    },
+    randomRolls: {
+        barrelPerks: {
+            type: [String],
+            required: true
+        },
+        magPerks: {
+            type: [String],
+            required: true
+        },
+        perks1: {
+            type: [String],
+            required: true
+        },
+        perks2: {
+            type: [String],
+            required: true
+        },
+        masterworks: {
+            type: [String],
+            required: true
+        }
+    },
+    ornaments: {
+        type: [String],
+        required: false
+    },
+    weaponImg: {
+        type: String,
+        required: false
+    }
+})
+
+module.exports = mongoose.model('Weapon', weaponSchema)
