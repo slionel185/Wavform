b1382afe3d986e3d2ff61e34aed102ff81833b7e
slionel185 <slionel1850@gmail.com>
2020-04-29 10:09:37 -0700

    Add files via upload

--- api/routes/weapon.route.js
+++ api/routes/weapon.route.js
@@ -4,7 +4,46 @@ const router = express.Router()
 const Weapon = require('../models/weapon.model')
 
 router.post('/new', (req, res) => {
-    
+    let newWeapon = new Weapon({
+        name: req.body.name,
+        weaponType: req.body.weaponType,
+        frame: {
+            name: req.body.frame
+        },
+        shownStats: {
+            impact: req.body.impact,
+            range: req.body.range,
+            stability: req.body.stability,
+            handling: req.body.handling,
+            reloadSpeed: req.body.reloadSpeed
+        },
+        hiddenStats: {
+            aimAssistance: req.body.aimAssistance,
+            inventorySize: req.body.inventorySize,
+            zoom: req.body.zoom,
+            recoil: req.body.recoil,
+            bounceIntensity: req.body.bounceIntensity,
+            bounceDirection: req.body.bounceDirection
+        },
+        curatedRoll: {
+            barrelPerk: req.body.curatedRollBarrelPerk,
+            magPerk: req.body.curatedRollMagPerk,
+            perk1: req.body.curatedRollPerk1,
+            perk2: req.body.curatedRollPerk2,
+            masterwork: req.body.curatedRollMasterwork
+        },
+        randomRolls: {
+            barrelPerks: req.body.randomRollsBarrelPerks.split('\r\n'),
+            magPerks: req.body.randomRollsMagPerks.split('\r\n'),
+            perks1: req.body.randomRollsPerks1.split('\r\n'),
+            perks2: req.body.randomRollsPerks2.split('\r\n'),
+            masterworks:req.body.randomRollsMasterworks.split('\r\n') 
+        },
+        weaponImg: req.body.weaponImg
+    }).save((err, weapon) => {
+        if(err) throw err
+        res.redirect('/')
+    })
 })
 
 router.get('/:name', (req, res) => {
